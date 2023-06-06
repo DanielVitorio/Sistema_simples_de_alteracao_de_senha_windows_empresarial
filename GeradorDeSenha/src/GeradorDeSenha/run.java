bf736ae8867578cc295987e11dc2058bab6fd029
Daniel Vitorio Augusto <tecnico.daniel@youvr.local>
2023-06-05 22:57:31 -0300

    first commit

---
+++ GeradorDeSenha/src/GeradorDeSenha/run.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package GeradorDeSenha;        
+
+/**
+ *
+ * @author tecnico.daniel
+ */
+public class run {
+
+    public static void main(String[] args) {
+       tela1 tela = new tela1();
+        tela.setVisible(true);
+    }
+}
